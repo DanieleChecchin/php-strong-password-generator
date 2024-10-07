84d0c6f03a716b9bbd3f47a08e041f7c900014f1
DanieleChecchin <danielechecchin26@gmail.com>
2024-10-07 17:02:04 +0200

    layout

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ require_once __DIR__ . '/functions/functions.php';
     <title>Random Password Generator</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 
 </head>
 
@@ -31,22 +32,22 @@ Verificato il corretto funzionamento del nostro codice, spostiamo la logica in u
  includeremo poi nella pagina principale. -->
 
     <main class="container">
-        <h1 class="fw-bold text-center mt-5">Strong Password Generator</h1>
-        <h2 class="fw-bold text-center my-2">Genera una password sicura </h2>
-
-        <form action="index.php" method="GET">
-            <div class="my-3">
-                <label for="password" class="form-label fw-bold">Lunghezza password</label>
-                <input type="number" class="form-control w-50" id="password" name="password"
-                    placeholder="Inserire la lunghezza della password">
-            </div>
-            <button class="btn btn-primary">Genera</button>
-        </form>
-
+        <h1 class="fw-bold text-center pt-5">Strong Password Generator</h1>
+        <h2 class="fw-bold text-center my-2 text-white">Genera una password sicura </h2>
 
         <div class="result my-4">
-            <h3> <?= getRandomPassword($_GET['password']) ?> </h3>
+            <h3>Password: <?= getRandomPassword($_GET['password']) ?> </h3>
         </div>
+
+        <form action="index.php" method="GET">
+            <div class="my-3 bg-light insert">
+                <label for="password" class="form-label fw-bold">Lunghezza password</label>
+                <input type="number" class="form-control w-50 my-4" id="password" name="password"
+                    placeholder="Inserire la lunghezza della password">
+                <button class="btn btn-primary my-5">Genera</button>
+            </div>
+
+        </form>
 
     </main>
 
