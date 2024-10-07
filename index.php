8a641cf59c210f2a820cde5e42507a87aebc3037
DanieleChecchin <danielechecchin26@gmail.com>
2024-10-07 15:14:29 +0200

    form created

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
     <title>Random Password Generator</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="/style.css">
 </head>
 
 <body>
@@ -27,8 +29,17 @@ Milestone 2
 Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che
  includeremo poi nella pagina principale. -->
 
-    <main class="bg-dark">
+    <main class="container">
+        <h1 class="fw-bold text-center mt-5">Strong Password Generator</h1>
+        <h2 class="fw-bold text-center my-2">Genera una password sicura </h2>
 
+        <form action="index.php" method="GET">
+            <div class="my-3">
+                <label for="password" class="form-label fw-bold">Lunghezza password</label>
+                <input type="password" class="form-control w-25" id="password"
+                    placeholder="Inserire la lunghezza della password">
+            </div>
+        </form>
     </main>
 
 
